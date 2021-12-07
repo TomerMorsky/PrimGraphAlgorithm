9b0521906ce83dda0d9bf78d9d044be50b7bf2e1
Tomer <morsky2@gmail.com>
2021-12-08 00:28:58 +0200

    Add max heap

---
+++ PrimeAlgorithem/PrimeAlgorithem/MaxHeap.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeAlgorithem
+{
+    public class MaxHeap<T> where T : IComparable<T>
+    {
+        #region Fields
+
+        private readonly List<T> _elements;
+        private int _size;
+
+        #endregion
+
+        #region C'tor
+        public MaxHeap(){}
+        #endregion
+
+        #region Public methods
+
+        public bool IsEmpty()
+        {
+            return _size == 0;
+        }
+
+        public T Peek()
+        {
+            if (_size == 0)
+                throw new IndexOutOfRangeException();
+
+            return _elements[0];
+        }
+
+        public T Pop()
+        {
+            if (_size == 0)
+                throw new IndexOutOfRangeException();
+
+            var result = _elements[0];
+            _elements[0] = _elements[_size - 1];
+            _size--;
+
+            ReCalculateDown();
+
+            return result;
+        }
+
+        public void Add(T element)
+        {
+            if (_size == _elements.Count)
+                throw new IndexOutOfRangeException();
+
+            _elements[_size] = element;
+            _size++;
+
+            ReCalculateUp();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private bool HasLeftChild(int elementIndex) => GetLeftChildIndex(elementIndex) < _size;
+        private bool HasRightChild(int elementIndex) => GetRightChildIndex(elementIndex) < _size;
+
+        private T GetLeftChild(int elementIndex) => _elements[GetLeftChildIndex(elementIndex)];
+        private T GetRightChild(int elementIndex) => _elements[GetRightChildIndex(elementIndex)];
+        private T GetParent(int elementIndex) => _elements[GetParentIndex(elementIndex)];
+
+        private void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = _elements[firstIndex];
+            _elements[firstIndex] = _elements[secondIndex];
+            _elements[secondIndex] = temp;
+        }
+
+        private void ReCalculateDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                var biggerIndex = GetLeftChildIndex(index);
+                if (HasRightChild(index) && GetRightChild(index).CompareTo(GetLeftChild(index)) > 0)
+                {
+                    biggerIndex = GetRightChildIndex(index);
+                }
+
+                if (_elements[biggerIndex].CompareTo(_elements[index]) < 0)
+                {
+                    break;
+                }
+
+                Swap(biggerIndex, index);
+                index = biggerIndex;
+            }
+        }
+
+        private void ReCalculateUp()
+        {
+            var index = _size - 1;
+            while (!IsRoot(index) && _elements[index].CompareTo(GetParent(index)) > 0)
+            {
+                var parentIndex = GetParentIndex(index);
+                Swap(parentIndex, index);
+                index = parentIndex;
+            }
+        }
+
+        #endregion
+
+        #region Static methods
+
+        private static int GetLeftChildIndex(int elementIndex) => 2 * elementIndex + 1;
+        private static int GetRightChildIndex(int elementIndex) => 2 * elementIndex + 2;
+        private static int GetParentIndex(int elementIndex) => (elementIndex - 1) / 2;
+        private static bool IsRoot(int elementIndex) => elementIndex == 0;
+
+        #endregion
+    }
+}
